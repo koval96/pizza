98d278456374e167ce6ead93740f6d76dcfcb1a0
jpcdevel <sashakovalenko96@yandex.ru>
2022-03-27 19:39:58 +0300

    slight alterations in settings file

--- api/api/settings.py
+++ api/api/settings.py
@@ -152,7 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://www.piz2a.netllify.app",
-    "https://piz2a.netllify.app",
+    "https://piz2a.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
