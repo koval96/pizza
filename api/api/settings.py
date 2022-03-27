ad2ef6b6ab2d8c05eb25467f4e614232f3814c74
jpcdevel <sashakovalenko96@yandex.ru>
2022-03-27 19:31:29 +0300

    requirements file chaged

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^3dnm!7!$riry2565o2m&k091bdyif^g2$d28k98-@rh-v^=km'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['piz2a.herokuapp.com', '127.0.0.1', 'localhost']
 
